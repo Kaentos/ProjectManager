d4cd6be6353857af52c1d32031f0e08baf75ac96
Kaentos <miguelmagueijo@hotmail.com>
2019-06-29 14:51:03 +0100

    Gets project data with get

--- php/sessionCheckTime.php
+++ php/sessionCheckTime.php
@@ -1,5 +1,6 @@
 <?php
     // Check if user hasn't do anything in 30min if he didn't do anything, destroys session
+    // If he didn't reach 30min reset session time
     if (time() - $_SESSION['user']['lastActivity'] > 1800) {
         session_destroy();   // destroy session data in storage
         header("Location: /projectmanager");
---
+++ project/index.php
@@ -0,0 +1,115 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["user"])){
+        header("Location: /projectmanager/");
+    } else {
+        include "$_SERVER[DOCUMENT_ROOT]/projectmanager/php/sessionCheckTime.php";
+        $dbHost = "localhost";
+        $dbUser = "root";
+        $dbPassword = "";
+        $dbName = "pmanager";
+        $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+        $UserData = array();
+        $query = "SELECT * FROM  user WHERE id=".$_SESSION["user"]["id"];
+        if ($result = $conn->query($query)) {
+            if ($result->num_rows == 1){
+                if ($row = $result->fetch_array(MYSQLI_ASSOC)){
+                    $UserData += ["id" => $row["id"]];
+                    $UserData += ["username" => $row["username"]];
+                    $UserData += ["role" => $row["role"]];
+                    $_SESSION["user"]["role"] = $row["role"];
+                } else {
+                    printf("MAJOR ERROR CAN'T CONVERT USER ROW TO ARRAY");
+                    die();
+                }
+            } else {
+                die();
+            }
+            $result->close();
+        } else {
+            printf("Error in select user query");
+            die();
+        }
+    }
+
+    if (isset($_GET["id"]) && is_numeric($_GET["id"])){
+        $projectID = $_GET["id"];
+    } else {
+        // header("location: /projectmanager/dashboard/projects.php");
+    }
+
+    if (isset($projectID)){
+        $projectData = getData($conn, $projectID, $UserData["id"]);
+        print_r($projectData);
+        if (isset($projectData)){
+            echo"asd";
+        } else {
+            // header("location: /projectmanager/dashboard/projects.php");
+        }
+    }
+
+    function getData($conn, $projectID, $userID){
+        // Get project data
+        $query = "SELECT p.*, s.name as Sname, u.username FROM projects AS p INNER JOIN pstatus AS s ON p.idStatus=s.id INNER JOIN projectmembers AS pm ON p.id = pm.idProject INNER JOIN user AS u ON p.idCreator = u.id WHERE p.id=$projectID AND pm.idUser=$userID;";
+        if ($result = $conn->query($query)) {
+            if ($result->num_rows == 1){
+                if($row = $result->fetch_array(MYSQLI_ASSOC)){
+                    return $row;
+                }
+            } elseif ($result->num_rows > 1) {
+                die("Error P2, report with error code and project name");
+            } else {
+                return;
+            }
+        } else {
+            die();
+        }
+    }
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height, target-densitydpi=medium-dpi" />
+        <title><?php echo $projectData["name"] ?></title>
+        <meta name="description" content="Project Manager">
+        <meta name="author" content="Miguel Magueijo">
+        <link rel="icon" href="/projectmanager/img/icon.png">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
+        <!-- Remove comment to get local fontawesome, comment link above -->
+        <!-- <link rel="stylesheet" href="/projectmanager/fontawesome/css/all.css"> -->
+        <link rel="stylesheet" href="/projectmanager/css/db.css">
+        <link rel="stylesheet" href="/projectmanager/css/Custom.css">
+        <link rel="stylesheet" href="/projectmanager/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="page-wrapper chiller-theme toggled">
+            <?php include "$_SERVER[DOCUMENT_ROOT]/projectmanager/sidebar/bar.php"; ?>
+
+
+            <main class="page-content">
+                <div class="container-fluid">
+                    <div>
+                        <span style="font-size:2rem; font-weight: 500;">{PROJECT NAME}</span>
+                        
+                    </div>
+                    <hr>
+                    
+                </div>
+            </main>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="/projectmanager/js/db.js"></script>
+        <script src="/projectmanager/js/bootstrap.min.js"></script>
+    </body>
+</html>
